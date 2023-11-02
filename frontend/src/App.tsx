83824d8253b958e98511861ce3afcd6db26a0627
gakshita <akshitagoyal1516@gamil.com>
2023-11-02 15:05:14 +0530

    added lazy loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 import CoinFlip from "@Views/CoinFlip";
-import MegaDice from "@Views/MegaDice";
+// import MegaDice from "@Views/MegaDice";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
+import { lazy, Suspense } from "react";
+
+const MegaDice = lazy(() => import("@Views/MegaDice"));
 function App() {
     const [currentTheme, setCurrentTheme] = useState("dark");
 
@@ -11,7 +14,14 @@ function App() {
         <Routes>
             <Route path="/" element={<Layout currentTheme={currentTheme} />}>
                 <Route path="coin-flip" element={<CoinFlip />} />
-                <Route path="mega-dice" element={<MegaDice />} />
+                <Route
+                    path="mega-dice"
+                    element={
+                        <Suspense fallback={<></>}>
+                            <MegaDice />
+                        </Suspense>
+                    }
+                />
 
                 <Route index element={<CoinFlip />} />
             </Route>
